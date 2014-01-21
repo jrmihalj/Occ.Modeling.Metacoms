eaa677a12765fe758e0f7ec9ff35ede6e8c951f7
jrmihalj <joseph.mihaljevic@colorado.edu>
2014-01-21 10:17:27 -0700

    not much changed

--- R/Pub_Code/Simulation_Table_Distributions.R
+++ R/Pub_Code/Simulation_Table_Distributions.R
@@ -28,18 +28,18 @@ AntiLogit <- function(x){
 
 # Establish community-level hyperparameters (if using normal distribution)
 # community-level probability of occurrence
-p_b <- 0.3 
-mu_b <- Logit(p_b)
-sd_b <- 0.5 # standard dev. b
+# p_b <- 0.3 
+# mu_b <- Logit(p_b)
+# sd_b <- 0.75 # standard dev. b
 
 
 # Establish species-specific base-line occurrence
 # I assume these base-line probabilities do not change over time.
 b.spp <- rep(Logit(0.6), N) # Fixed occurrence
-#b.spp <- rnorm(N, mu_b, sd_b) # Normal distribution 
-#b.spp <- runif(N, 0, 1) # Uniform distribution
+# b.spp <- rnorm(N, mu_b, sd_b) # Normal distribution 
+# b.spp <- runif(N, 0, 1) # Uniform distribution
 
-# Establish species-specific detection probabilities
+# # Establish species-specific detection probabilities
 rho.spp <- rep(0.7, N) # Fixed detection prob. 
 
 
@@ -47,13 +47,19 @@ rho.spp <- rep(0.7, N) # Fixed detection prob.
 # I will assume that occupancy is only potentially related to one covariate for each species.
 # betas.b <- rep(0, N)
 # betas.b <- rnorm(N, 0, .5)
-betas.b <- runif(N, -1, 1)
+ betas.b <- runif(N, -1, 1)
+
+# For nested patterns:
+#betas.b <- runif(N, -1, 1)
 
 # Establish covariate values:
 # I will assume these values do not change over the time-scale of the multiple observations.
-X <- rnorm(K, 0, 5) # Normal dist.
-#X <- runif(K, -4, 4) # Uniform dist.
+#X <- rnorm(K, 0, 2) # Normal dist.
+X <- runif(K, -3, 3) # Uniform dist.
 
+# For nested patterns:
+#X <- runif(K, 0, 3)
+#X <- rnorm(K, 2, 1)
 
 # Initial occupancy state
 z <- array(0, dim = c(N, K)) # This will hold occupancy states
