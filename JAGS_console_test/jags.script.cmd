e98f378524cadda4df942cbf73d04bd96de45ce0
Joseph R. Mihaljevic <joseph.mihaljevic@colorado.edu>
2014-05-07 16:22:40 -0600

    no longer need to change directory from within the JAGS command script file

--- JAGS_console_test/jags.script.cmd
+++ JAGS_console_test/jags.script.cmd
@@ -1,6 +1,5 @@
 #jags.test file for simulating metacommunities
 
-cd "/Users/JRMihaljevic/Documents/Thesis Research/Occ.Model.EMS/JAGS_console_test"
 model in "OccMod_SingleYear.txt"
 data in "data.R"
 compile, nchains(3)
